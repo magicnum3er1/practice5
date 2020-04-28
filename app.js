84a70f63fd38e1d5c6a04ad7515f706b96bec566
majorstirrer <magickotlovski@gmail.com>
2020-04-28 08:34:32 +0100

    od poczatku

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('We are in!');
+app.set("view engine", "ejs")
+
+
+app.get('/', function(req, res){
+    res.send('connected');
 });
+
+
+app.get('/home', function(req, res){
+    res.render('home');
+});
+
+app.get('/page1', function(req, res){
+    res.render('page1');
+});
+
+
+
 
 app.listen(3000, () => console.log('server connected'));
